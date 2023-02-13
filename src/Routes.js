6f602690e731272b6bb3621e995072666f646cb4
preetsimon <preetsimon@gmail.com>
2023-02-13 21:00:59 +0000

    Code Synced by DhiWise

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import SmartHome from "pages/SmartHome";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "pages/Home";
+import NotFound from "pages/NotFound";
+const ProjectRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/smarthome" element={<SmartHome />} />
+      </Routes>
+    </Router>
+  );
+};
+export default ProjectRoutes;
